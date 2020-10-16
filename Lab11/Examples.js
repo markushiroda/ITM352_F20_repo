8dea3ce3fcb79251bac697e49e2469b60e3a9c65
markushiroda <markdu@hawaii.edu>
2020-10-16 01:25:39 -1000

    update

---
+++ Lab11/Examples.js
@@ -0,0 +1,9 @@
+age = 20;
+name = "Mark";
+attributes  =  name + ";" + age + ";" + (age + 0.5) + ";" + (0.5 - age);
+parts = attributes.split (';');
+for(i in parts) {
+    parts[i] = `${typeof parts[i]} ${parts[i]}`;
+}
+
+console.log(parts.join(","));
