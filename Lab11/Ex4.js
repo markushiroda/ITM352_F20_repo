fdb21fd57ff0ae0069d00b2d8d4ae05b22953b35
markushiroda <markdu@hawaii.edu>
2020-10-22 10:35:16 -1000

    Create Ex4.js
    update

---
+++ Lab11/Ex4.js
@@ -0,0 +1,23 @@
+function isNoneNegIntString(string_to_check, returnErrors=false){
+    // this function returns true if string_to_check is a non negative integer. If returnErrorstrue it will return the array of reasons it is not a non-negative integer
+    errors = []; // assume no errors at first
+    if(Number(string_to_check) != string_to_check) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(string_to_check) != string_to_check) errors.push('Not an integer!'); // Check that it is an integer
+
+        return returnErrors ? errors : (errors.length == 0);
+}
+age = 20;
+name = "Mark";
+attributes  =  name + ";" + age + ";" + (age + 0.5) + ";" + (0.5 - age);
+pieces = attributes.split (';');
+
+function callback(i, part) {
+    console.log(`${pieces[i]} is non neg int ${isNonNegIntString(part, true).join("***")}`);
+}
+
+pieces.forEach( );
+    console.log(`${part} is non neg int ${isNonNegIntString(pieces[i],true).join("***")}`);
+
+
+pieces.forEach(callback);
