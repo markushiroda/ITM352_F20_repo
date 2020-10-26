ef402d3a5ee6983e1bab3e03e097ecdd2e63ea81
markushiroda <markdu@hawaii.edu>
2020-10-25 22:30:54 -1000

    updates

--- Lab11/Ex4.js
+++ Lab11/Ex4.js
@@ -1,11 +1,10 @@
-function isNoneNegIntString(string_to_check, returnErrors=false){
+function isNonNegIntString(string_to_check, returnErrors=false){
     // this function returns true if string_to_check is a non negative integer. If returnErrorstrue it will return the array of reasons it is not a non-negative integer
     errors = []; // assume no errors at first
     if(Number(string_to_check) != string_to_check) errors.push('Not a number!'); // Check if string is a number value
-    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(string_to_check < 0) errors.push('Negative value!'); // Check if it is non-negative
     if(parseInt(string_to_check) != string_to_check) errors.push('Not an integer!'); // Check that it is an integer
-
-        return returnErrors ? errors : (errors.length == 0);
+    return returnErrors ? errors : ((errors.length > 0)?false:true);
 }
 age = 20;
 name = "Mark";
