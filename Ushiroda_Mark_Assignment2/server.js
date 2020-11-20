eb888e1e09ebb99d7a02643107fad80b54228043
markushiroda <markdu@hawaii.edu>
2020-11-20 11:13:35 -1000

    update

--- Ushiroda_Mark_Assignment2/server.js
+++ Ushiroda_Mark_Assignment2/server.js
@@ -71,7 +71,7 @@ app.post("/process_login", function (req, res) {
             req.query.username = the_username;
             console.log(users_reg_data[req.query.username].name);
             req.query.name = users_reg_data[req.query.username].name
-            res.redirect('/Invoice.html?' + queryString.stringify(req.query));
+            res.redirect('/invoice.html?' + queryString.stringify(req.query));
             return;
             //Redirect them to invoice here if they logged in correctly//
         } else {
